9a712ea9dc890381de50760fc6f59d242aacb012
DiverGent <gamer.felixbook@gmail.com>
2022-12-14 00:53:22 +0300

    Решение домашек

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+    public static void main(String[] args) {
+
+       System.out.println("Task 3.1");
+
+        for (int i = 1; i<= 10; i++) {
+
+
+            System.out.println(i);
+        }
+            System.out.println("Task 3.2");
+
+
+        for (int t = 10; t>= 1; t--) {
+
+
+            System.out.println(t);
+        }
+
+
+        System.out.println("Task 3.3");
+
+
+
+        for (int j =0; j < 17; j=j+2) {
+
+            System.out.println(j);
+          }
+
+
+
+        System.out.println("Task 3.4");
+
+        for (int i = 10; i >=-10; i--) {
+            System.out.println(i);
+        }
+
+
+        System.out.println("Task 3.5");
+
+        
+        for (int i = 1906; i <=2096 ; i = i + 4) {
+
+
+            System.out.println(i+ " год является високосным");
+        }
+
+
+        System.out.println("Task 3.6");
+
+
+        
+        for (int i = 7; i <= 98; i= i+7) {
+            System.out.println(i);
+        }
+
+        System.out.println("Task 3.7");
+
+        for (int i = 1; i <=512; i= i*2) {
+            System.out.println(i);
+        }
+        
+        
+        
+
+    }
+}
